0a63b176aa4716ad3711f3d22f6484f304c1b6a7
Sanjay-Narayanan <148704833+sanjay-narayanan-12@users.noreply.github.com>
2025-12-20 18:05:50 +0530

    Intial commit (#1)
    Co-authored-by: sanjay-narayanan-12 <demous9702@gmail.com>

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World from Go Cloud Run!")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", handler)
+	log.Println("Server started on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
